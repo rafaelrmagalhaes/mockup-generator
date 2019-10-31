f4d408d2078bdcfab21f31a1953abb630b0fa7cb
Rafael Magalhães <rafael.magalhaes@corp.globo.com>
2019-10-30 22:31:02 -0300

    Updates

---
+++ precache-manifest.a29e56cfe4599341cff1ae3d64bcc4dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b90a9bd2278d145d639ada8b607c62",
+    "url": "/mockup-generator/index.html"
+  },
+  {
+    "revision": "2c054bd1e1265394788b",
+    "url": "/mockup-generator/static/css/main.4fceaf6f.chunk.css"
+  },
+  {
+    "revision": "61958df5f46a04ba84fc",
+    "url": "/mockup-generator/static/js/2.96d53459.chunk.js"
+  },
+  {
+    "revision": "2c054bd1e1265394788b",
+    "url": "/mockup-generator/static/js/main.e12e022d.chunk.js"
+  },
+  {
+    "revision": "67536244984f3f7316ff",
+    "url": "/mockup-generator/static/js/runtime-main.be0fe19c.js"
+  },
+  {
+    "revision": "ad59608ef5171e4cfc7947c323f4c97a",
+    "url": "/mockup-generator/static/media/camisa_costas.ad59608e.png"
+  },
+  {
+    "revision": "527c098bc3404754b55b7f37e633894b",
+    "url": "/mockup-generator/static/media/camisa_frente.527c098b.png"
+  },
+  {
+    "revision": "aa35bae632e921b8b3ce729c0ec7adff",
+    "url": "/mockup-generator/static/media/marca_dagua.aa35bae6.png"
+  }
+]);
