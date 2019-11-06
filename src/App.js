00abdc0f08383a4212cbcf195c8ebb10c237a26f
Rafael Magalhães <rafael.magalhaes@corp.globo.com>
2019-11-06 16:44:13 -0300

    print button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import './App.scss';
-import Preview from './preview';
-import Bar from './bar';
-import marca_dagua from './marca_dagua.png';
-import camisa_frente from './camisa_frente.png';
-import camisa_costas from './camisa_costas.png';
+import React, { Component } from "react";
+import html2canvas from "html2canvas";
+import "./App.scss";
+import Preview from "./preview";
+import Bar from "./bar";
+import marca_dagua from "./marca_dagua.png";
+import camisa_frente from "./camisa_frente.png";
+import camisa_costas from "./camisa_costas.png";
 
 export default class App extends Component {
   constructor() {
     super();
-  
+
     this.state = {
-      color: '#fff',
+      color: "#fff",
       frontData: null,
       frontData2: null,
       backData: null,
       backData2: null,
+      clientName: ""
     };
   }
 
@@ -25,24 +27,61 @@ export default class App extends Component {
     });
   }
 
+  print() {
+    html2canvas(document.getElementById("canvas")).then(canvas => {
+      var a = document.createElement("a");
+      a.href = canvas.toDataURL("image/jpeg");
+      a.download = `mockup_${this.state.clientName}.jpg`;
+      a.click();
+    });
+  }
+
+  updateClientName(e) {
+    this.setState({
+      clientName: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="main">
-        <img src={marca_dagua} className="watermark" alt="Estampa Personalizada" />
-        <div className="mockup" style={{backgroundColor: this.state.color}}>
-          <div className="preview" style={{ backgroundImage: `url(${camisa_frente})` }}>
-            <Preview data={this.state.frontData} />
-            <Preview data={this.state.frontData2} />
-          </div>
-          <div className="preview" style={{ backgroundImage: `url(${camisa_costas})` }}>
-            <Preview data={this.state.backData} />
-            <Preview data={this.state.backData2} />
+        <a href="#" className="print" onClick={() => this.print()}>
+          Imprimir
+        </a>
+        <div id="canvas">
+          <img
+            src={marca_dagua}
+            className="watermark"
+            alt="Estampa Personalizada"
+          />
+          <div className="mockup" style={{ backgroundColor: this.state.color }}>
+            <div
+              className="preview"
+              style={{ backgroundImage: `url(${camisa_frente})` }}
+            >
+              <Preview data={this.state.frontData} />
+              <Preview data={this.state.frontData2} />
+            </div>
+            <div
+              className="preview"
+              style={{ backgroundImage: `url(${camisa_costas})` }}
+            >
+              <Preview data={this.state.backData} />
+              <Preview data={this.state.backData2} />
+            </div>
           </div>
         </div>
 
+        <input
+          type="text"
+          placeholder="Nome do Cliente"
+          className="client"
+          onChange={e => this.updateClientName(e)}
+        />
+
         <div className="color">
           <label htmlFor="color">Cor da camisa:</label>
-          <select id="color" onChange={(e) => this.changeColor(e)}>
+          <select id="color" onChange={e => this.changeColor(e)}>
             <option value="#fff">Branca</option>
             <option value="#f00">Vermelha</option>
             <option value="#00f">Azul</option>
@@ -51,12 +90,28 @@ export default class App extends Component {
           </select>
         </div>
 
-        <hr/>
+        <hr />
 
-        <Bar position="left" index={1} onUpdate={ frontData => this.setState({ frontData }) } />
-        <Bar position="left" index={2} onUpdate={ frontData2 => this.setState({ frontData2 }) } />
-        <Bar position="right" index={2} onUpdate={ backData2 => this.setState({ backData2 }) } />
-        <Bar position="right" index={1} onUpdate={ backData => this.setState({ backData }) } />
+        <Bar
+          position="left"
+          index={1}
+          onUpdate={frontData => this.setState({ frontData })}
+        />
+        <Bar
+          position="left"
+          index={2}
+          onUpdate={frontData2 => this.setState({ frontData2 })}
+        />
+        <Bar
+          position="right"
+          index={2}
+          onUpdate={backData2 => this.setState({ backData2 })}
+        />
+        <Bar
+          position="right"
+          index={1}
+          onUpdate={backData => this.setState({ backData })}
+        />
       </div>
     );
   }
